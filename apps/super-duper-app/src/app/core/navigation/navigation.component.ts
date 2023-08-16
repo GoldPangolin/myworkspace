b90ac833ed83c77b73ccc16de58d5c45247ed07c
andrew Mac <andrew.kolansky@gmail.com>
2023-08-16 15:17:36 -0600

    add navigation sp animations and theme color

--- apps/super-duper-app/src/app/core/navigation/navigation.component.ts
+++ apps/super-duper-app/src/app/core/navigation/navigation.component.ts
@@ -1,8 +1,9 @@
 /** this isn't the way I want to import this. think I need to turn those animations into a module */
-import { Component, ElementRef, EventEmitter, Output, Renderer2, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewEncapsulation } from '@angular/core';
 import { NavigationItem } from './navigation.types';
 import { SuperDuperAnimationsModule } from 'libs/common-ui/src/lib/animations/animations.module';
 import { defaultNavigation } from './mock-data/data';
+import { AnimationBuilder, AnimationPlayer, animate, style } from '@angular/animations';
 @Component({
   selector: 'side-navigation',
   templateUrl: './navigation.component.html',
@@ -12,21 +13,19 @@ import { defaultNavigation } from './mock-data/data';
 })
 export class NavigationComponent {
   mainNavigation: NavigationItem[] = [];
-  asideNavigation: NavigationItem | NavigationItem[];
-  activeAsideItem: NavigationItem;
+  asideNavigation?: NavigationItem | NavigationItem[];
+  activeAsideItem: NavigationItem | undefined;
 
-  @Output() closeAsideNavEvent: EventEmitter<null> = new EventEmitter<null>();
-
+  private player?: AnimationPlayer;
   private overlay?: HTMLElement;
   private readonly _handleOverlayClick: any;
 
   constructor(
+    private animationBuilder: AnimationBuilder,
     private elementRef: ElementRef,
     private renderer: Renderer2
     ) {
       this.mainNavigation = defaultNavigation;
-      this.activeAsideItem = defaultNavigation[0];
-      this.asideNavigation = defaultNavigation[0];
       this._handleOverlayClick = (): void =>
       {
           this.close();
@@ -48,8 +47,14 @@ export class NavigationComponent {
     if(!this.overlay) {
       return;
     } 
-    this.closeAsideNavEvent.emit();
+    this.player = this.animationBuilder.build([
+      animate('300ms cubic-bezier(0.25, 0.8, 0.25, 1)', style({opacity: 0.2})),
+    ]).create(this.overlay);
+    this.player.play();
+
+    this.activeAsideItem = undefined;
     this.overlay?.parentNode?.removeChild(this.overlay);
+
     this.overlay = undefined;
 
   }
@@ -64,6 +69,10 @@ export class NavigationComponent {
     this.overlay?.classList.add('navigation-overlay');
     // append overlay to specific location.
     this.renderer.appendChild(this.elementRef.nativeElement.parentElement, this.overlay);
+    this.player = this.animationBuilder.build([
+      animate('300ms cubic-bezier(0.25, 0.8, 0.25, 1)', style({opacity: .2})),
+    ]).create(this.overlay);
+    this.player.play();
     // add event listener to it so that it closes the side panel.
     this.overlay?.addEventListener('click', this._handleOverlayClick);
   }
